ef84a39fa8ffb3fcd2731d79bd19e925cbc07f56
Stanislav Katkov <sk@skylup.com>
2021-08-09 20:02:43 +0200

    Adding bank accounts

--- src/clojure_bank/handler.clj
+++ src/clojure_bank/handler.clj
@@ -1,12 +1,32 @@
 (ns clojure-bank.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [clojure.data.json :as json]
+            [clojure.pprint :as pp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn get-account [id] (str "The user ID is " id))
+(def account-collection (atom []))
+
+(defn add-account [name]
+  (swap! account-collection conj {:name name}))
+
+(defn get-account [id]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body  (str (json/write-str @account-collection))})
+
+(defn getparameter [req pname] (get (:params req) pname))
+
+(defn add-account-handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (->
+          (let [p (partial getparameter req)]
+            (str (json/write-str (add-account (p :name))))))})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/account" [] add-account-handler)
   (GET "/account/:id" [id] (get-account id))
   (route/not-found "Not Found"))
 
--- test/clojure_bank/handler_test.clj
+++ test/clojure_bank/handler_test.clj
@@ -1,9 +1,10 @@
 (ns clojure-bank.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.pprint :as pp]
             [clojure-bank.handler :refer :all]))
 
-(deftest test-app
+(deftest main-routes
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
@@ -12,3 +13,15 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+(deftest app-accounts
+  (testing "no accounts"
+    (let [response (app (mock/request :get "/account/1"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "[]"))))
+
+  (testing "adding an account"
+    (app (mock/request :get "/account?name=test1"))
+
+    (let [response (app (mock/request :get "/account/1"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "[{\"name\":\"test1\"}]")))))

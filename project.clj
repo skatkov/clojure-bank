21cf77f667d038e8bc29022cecaca4273573e7b3
Stanislav Katkov <sk@skylup.com>
2021-08-09 20:02:28 +0200

    adding cljfmt

--- project.clj
+++ project.clj
@@ -6,11 +6,9 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.1.2"]
-                 [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.2.3"]
-                 [com.h2database/h2 "1.3.168"]
-                 [cheshire "4.0.3"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [org.clojure/data.json "0.2.6"]
+                 ]
+  :plugins [[lein-ring "0.12.5"][lein-cljfmt "0.8.0"]]
   :ring {:handler clojure-bank.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
